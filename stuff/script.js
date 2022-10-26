7df6bad9e0f07da59769afc11a7f57d4f60b7693
Tymari Veloso <tymariveloso@gmail.com>
2022-10-26 16:39:14 -0700

    added leaflet map

--- stuff/script.js
+++ stuff/script.js
@@ -1,7 +1,17 @@
-//get user location
-async function getCoords(){
-	pos = await new Promise((resolve, reject) => {
-		navigator.geolocation.getCurrentPosition(resolve, reject)
-	})
-    return [pos.coords.latitude, pos.coords.longitude]
+async function main(){
+    //get user location
+    async function getCoords(){
+        pos = await new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(resolve, reject)
+        })
+        return [pos.coords.latitude, pos.coords.longitude]
+    }
+    //make map
+    const map = L.map('map').setView([51.505, -0.09], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
 }
+main()
