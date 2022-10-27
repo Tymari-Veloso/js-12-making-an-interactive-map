afae49f8d45596eddfe9972fd7782e5d84a5e425
Tymari Veloso <tymariveloso@gmail.com>
2022-10-26 17:31:19 -0700

    added buildMap function, & bussiness-type button

--- stuff/script.js
+++ stuff/script.js
@@ -1,17 +1,43 @@
-async function main(){
+ async function main(){
     //get user location
-    async function getCoords(){
+    async function getUserLocation(){
         pos = await new Promise((resolve, reject) => {
             navigator.geolocation.getCurrentPosition(resolve, reject)
         })
         return [pos.coords.latitude, pos.coords.longitude]
     }
-    //make map
-    const map = L.map('map').setView([51.505, -0.09], 13);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-
+    //map object
+    const myMap = {
+        coordinates: [],
+        bussiness: [],
+        map: {},
+        markers: {},
+        //map constructor
+        buildMap(){
+            this.map = L.map('map').setView(this.coordinates,15)
+            //openstreetmap tiles
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 17,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(this.map);
+            //map markers
+            const marker = L.marker(this.coordinates)
+            marker
+            .addTo(this.map)
+            .bindPopup('<p1><b>You are here</b><br></p1>')
+        }
+    }
+    //bussiness type
+    document.getElementById('submit').addEventListener('click', async (event) => {
+        let business = document.getElementById('business').value
+        console.log(business)
+        })
+    //window load
+    window.onload = async () =>{
+        const coords = await getUserLocation()
+        console.log(coords)
+        myMap.coordinates = coords
+        myMap.buildMap()
+    }
 }
 main()
